d7f0b74c38fe196c404154dcbc8e0b7ab8206f26
Ndeyendour <ndeyefatou023@gmail.com>
2025-02-01 00:31:41 +0000

    correc

--- keurguiapi/server.js
+++ keurguiapi/server.js
@@ -21,8 +21,7 @@ const mongoURI = process.env.MONGODB_URL || "mongodb://localhost:27017/keurgui";
 
 mongoose
   .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
   })
   .then(() => console.log("✅ Connexion réussie à MongoDB"))
   .catch((err) => {
